82692ce9b2040f2ec78520e5e93645d90e045eda
Bruno-Hirono <2104619@aluno.univesp.br>
2024-01-12 10:35:55 -0300

    includes non random chosen numbers list

--- jogoMega.py
+++ jogoMega.py
@@ -1,7 +1,13 @@
 import random
 
-def CreateLottery(numbers):
-    lottery = []
+
+'''Brazilian main lottery (mega sena) is composed of 6 numbers chosen between 1 and 60. '''
+def CreateLotteryNumbers(numbers=6, list=[]):
+    "Creates a random lottery numbers list. Variables are the quantity of numbers (min = 6; max = 15) and a list of \
+    chosen numbers (non-random)"
+    if numbers < 6 or numbers > 15:
+        return "Avaliable lottery numbers are limited to a quantity between 06 and 15."
+    lottery = list
     while len(lottery) < numbers:
         chosenNumber = random.randint(1,60)
         if chosenNumber in lottery:
@@ -11,4 +17,5 @@ def CreateLottery(numbers):
     lottery.sort()
     return lottery
     
-print(CreateLottery(6))
+
+#print(CreateLotteryNumbers(6))
